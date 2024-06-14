6f71b2c2aea508a35b1a2f594039dc1ddd912d87
Jhosua Armando Putra Panjaitan <123630202+jhopan@users.noreply.github.com>
2024-06-15 00:44:29 +0800

    Update Tugas TA.py

--- Tugas TA.py
+++ Tugas TA.py
@@ -2,29 +2,30 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# to store data from POST method
-sensor_data_list = []
+@app.route('/api/sensor_data', methods=['POST'])
+def receive_sensor_data():
+    if request.is_json:
+        data = request.get_json()
+        temperature = data.get('temperature')
 
-@app.route('/sensor/data', methods=['POST'])
-def sensor_data():
-  # Get the data from the request
-  data = request.get_json()
-  temperature = data['temperature']
-  humidity = data['humidity']
-  timestamp = data['timestamp']
-
-  # store the sensor data
-  sensor_data_list.append({'temperature': temperature, 'humidity': humidity, 'timestamp': timestamp})
-
-  # Response message
-  response = {'message': 'Data received successfully!'}
-
-  # Return the response
-  return jsonify(response)
-
-@app.route('/sensor/data', methods=['GET'])
-def get_sensor_data():
-  return jsonify(sensor_data_list)
+        if temperature is not None:
+            response = {
+                'status': 'success',
+                'message': f'Temperature received: {temperature}'
+            }
+            return jsonify(response), 200
+        else:
+            response = {
+                'status': 'fail',
+                'message': 'Temperature data is missing'
+            }
+            return jsonify(response), 400
+    else:
+        response = {
+            'status': 'fail',
+            'message': 'Request body must be JSON'
+        }
+        return jsonify(response), 400
 
 if __name__ == '__main__':
-  app.run()
+    app.run(host='0.0.0.0', port=5000)
